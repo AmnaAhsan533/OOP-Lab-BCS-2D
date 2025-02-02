d423b585ece1dbd7aa7a936d90bf1591779b4312
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-02 17:12:05 +0500

    Update q2.cpp

--- Lab2/q2.cpp
+++ Lab2/q2.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 
-int add(int *arr[], int n){
-    for(int i=0; i<n; i++){
-        arr[i] = arr[i] + 2;
+void* add(void *arr_void_ptr){
+    int* arr = (int*)arr_void_ptr;
+    for(int i=0; i<5; i++){
+        arr[i] += 2;
     }
     return (void*)arr;
 }
 
-int main(){
-    int arr;
+int main(int argc, char *argv[]){
+    int arr[5];
+    if(argc < 6){
+        cout << "Usage: " << argv[0] << " num1 num2 num3 num4 num5" << endl;
+        return 1;
+    }
+    for(int i=0; i<5; i++){
+        arr[i] = atoi(argv[i+1]);
+    }
+    void* ptr = (void*)arr;
+    void* ret = add(ptr);
+    int *result = (int*)ret;
+    cout <<"Modified Array: ";
+    for(int i=0; i<5; i++){
+        cout<<result[i]<<" ";
+    }
 }
