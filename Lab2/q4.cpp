d8cec637ea80f6529516467567df9217bbb22cdb
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-02 17:12:38 +0500

    Update q4.cpp

--- Lab2/q4.cpp
+++ Lab2/q4.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+struct Employee{
+    int emp_id;
+    string emp_name;
+    double salary;
+};
 struct Organisation{
     string org_name;
     string org_number;
-    
-};
-struct Employee{
-        int emp_id;
-        string emp_name;
-        float salary;
-
-    };
-int main(){
-    struct Organisation org;
     struct Employee emp;
-    cin>>org.org_name>>org.org_number;
-    getline(org.)
+};
+int main(){
+    Organisation org;
+    org.org_name = "NU-Fast";
+    org.org_number = "NUFAST123ABC";
+    org.emp.emp_id = 127;
+    org.emp.emp_name = "Linus Sebastian";
+    org.emp.salary = 400000;
+
+    cout << "The size of structure organisation : " << sizeof(Organisation) << endl;
+    cout << "Organisation Name : " << org.org_name << endl;
+    cout << "Organisation Number : " << org.org_number << endl;
+    cout << "Employee id : " << org.emp.emp_id << endl;
+    cout << "Employee name : " << org.emp.emp_name << endl;
+    cout << "Employee Salary : " << org.emp.salary << endl;
 }
