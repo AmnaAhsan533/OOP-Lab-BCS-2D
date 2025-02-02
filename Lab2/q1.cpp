9fe1a1ab99db1b89e98ec67f7bc2be5e44968608
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-02 17:11:31 +0500

    Update q1.cpp

--- Lab2/q1.cpp
+++ Lab2/q1.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    // if(argc <2){
-    //     cout<<"Usage: "<<argv[0]<<"<list of numbers>"<<endl;
-    //     return 1;
-    // }
+    if(argc <2){
+        cout<<"Usage: "<<argv[0]<<"<list of numbers>"<<endl;
+        return 1;
+    }
     int n;
     n = argc -1;
     int arr[n];
