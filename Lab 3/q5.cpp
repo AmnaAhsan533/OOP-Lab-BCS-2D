88071956475ddfd59aba6052d9b574da606ecd40
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-05 16:04:07 +0500

    Update q5.cpp

--- Lab 3/q5.cpp
+++ Lab 3/q5.cpp
@@ -8,17 +8,43 @@ class Book{
     string author;
     string publisher;
 
-    string getBookInfo(string name, string ISBN, string author, string publisher){
+    Book(){} //default constructor
+    Book(string name, string ISBN, string author, string publisher){
         this->name = name;
         this->author = author;
         this->ISBN = ISBN;
-        this->publisher = publisher;
-        
+        this->publisher = publisher;       
+    }
+    string getBookName(){
+        return name;
+    }
+    string getISBN(){
+        return ISBN;
+    }
+    string getAuthor(){
+        return author;
+    }
+    string getPublisher(){
+        return publisher;
+    }
+    string getBookInfo() const{
+        return "Book Name: "+ name +"\nISBN: "+ ISBN +"\nAuthor: "+author +"\nPublisher: "+ publisher +"\n";
     }
 };
 
 int main(){
-    Book books[5];
+    Book books[5]={
+        Book("C", "123", "XYZ", "ABC-PUB"),
+        Book("C++", "456", "ABC", "DEF-PUB"),
+        Book("Java", "789", "LMN", "GHI-PUB"),
+        Book("Python", "321", "PQR", "JKL-PUB"),
+        Book("JavaScript", "654", "UVW", "MNO-PUB")
+    };
+
+    for(const auto& book: books){
+        cout<<book.getBookInfo()<<"\n";
+    }
+    
 
     return 0;
 }
