2f51bcfe59e150630c57fb380544a66ebfb15088
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-05 16:04:51 +0500

    Update q6.cpp

--- Lab 3/q6.cpp
+++ Lab 3/q6.cpp
@@ -1,44 +1,94 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Matrix{
-    public:
-    int rows;
-    int cols;
-    int elements[rows][cols];
+class Matrix {
+private:
+    int rows, cols;
+    vector<vector<int>> elements; 
 
-    Matrix(int r, int c){
+public:
+    Matrix(int r, int c) {
         rows = r;
         cols = c;
+        elements = vector<vector<int>>(r, vector<int>(c, 0));
     }
 
-    int getRows(){
-        return rows;
-    }
-    int getCols(){
-        return cols;
-    }
-    void setElements(int rows, int cols, int elements[][]){
+    int getRows() const { return rows; }
+    int getCols() const { return cols; }
 
+    void setElement(int i, int j, int value) {
+        if (i >= 0 && i < rows && j >= 0 && j < cols)
+            elements[i][j] = value;
+        else
+            cout << "Invalid index!\n";
     }
-    void add(){
-        if(rows == cols){
-            for(int i=0; i<rows; i++){
-                for(int j=0; j<cols; j++){
-                    
-                }
-            }
 
-        } else {
-            cout<<"Matrices cannot be added!"<<endl;
+    void printMatrix() const {
+        for (const auto &row : elements) {
+            for (int val : row)
+                cout << val << " ";
+            cout << endl;
         }
     }
-    void multiply(){
 
+    Matrix add(const Matrix &other) const {
+        if (rows != other.rows || cols != other.cols) {
+            cout << "Matrices cannot be added\n";
+            return Matrix(0, 0); // Return empty matrix
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                result.elements[i][j] = elements[i][j] + other.elements[i][j];
+
+        return result;
+    }
+
+    Matrix multiply(const Matrix &other) const {
+        if (cols != other.rows) {
+            cout << "Matrices cannot be multiplied\n";
+            return Matrix(0, 0); 
+        }
+
+        Matrix result(rows, other.cols);
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < other.cols; ++j)
+                for (int k = 0; k < cols; ++k)
+                    result.elements[i][j] += elements[i][k] * other.elements[k][j];
+
+        return result;
     }
 };
 
-int main(){
+int main() {
+    Matrix A(2, 2);
+    Matrix B(2, 2);
+
+    A.setElement(0, 0, 1);
+    A.setElement(0, 1, 2);
+    A.setElement(1, 0, 3);
+    A.setElement(1, 1, 4);
+
+    B.setElement(0, 0, 5);
+    B.setElement(0, 1, 6);
+    B.setElement(1, 0, 7);
+    B.setElement(1, 1, 8);
+
+    cout << "Matrix A:\n";
+    A.printMatrix();
+    
+    cout << "\nMatrix B:\n";
+    B.printMatrix();
+
+    cout << "\nMatrix A + B:\n";
+    Matrix C = A.add(B);
+    C.printMatrix();
+
+    cout << "\nMatrix A * B:\n";
+    Matrix D = A.multiply(B);
+    D.printMatrix();
 
     return 0;
 }
