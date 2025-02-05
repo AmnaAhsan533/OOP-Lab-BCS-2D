5363ab6ac8dbc88dc25940422665a796dcdcf625
Amna Ahsan <amnaahsan533@gmail.com>
2025-02-05 11:58:36 +0500

    Update q3.cpp

--- Lab 3/q3.cpp
+++ Lab 3/q3.cpp
@@ -7,32 +7,41 @@ class Glass{
         int LiquidLevel = 200;
     void Drink(int milliliters){
         LiquidLevel = 200 - milliliters;
+        if (LiquidLevel < 0) {
+            cout<<"Invalid input!"<<endl;
+        } else {
+            cout<<"Drank: "<<milliliters<<"ml. Remaining: "<<LiquidLevel<<"ml."<<endl;
+        }
+        
         if(LiquidLevel < 100 && LiquidLevel >= 0){
             Refill();
         }
     }
     void Refill(){
         LiquidLevel = 200;
-        cout<<"Glass refilled!"<<endl;
+        cout<<"Glass refilled to 200ml."<<endl;
     }
 };
 
 int main(int argc, const char* argv[]){
     Glass glass;
-    cout<<"Enter amout of liter: (-1 to stop)"<< endl;
+    //cout<<"Enter amout of liter: (-1 to stop)"<< endl;
     while(1){
-        int value;
-        cin>>value;
-        glass.Drink(value);
-        if(value == -1){
-            return 1;
-        }
-        // int i=1;
-        // if(atoi(argv[i]) == 0){
+        // int value;
+        // cin>>value;
+        
+        // if(value == -1){
         //     return 1;
-        //     }
-        // glass.Drink(atoi(argv[i]));
-        // i++;
+        // }
+        // glass.Drink(value);
+        for(int i=1; i<argc; i++){
+            int value = atoi(argv[i]);
+            if(value >= 0){
+                glass.Drink(value);
+            } else{
+                return 1;
+            }
+        }
     }   
     return 0;
 }
