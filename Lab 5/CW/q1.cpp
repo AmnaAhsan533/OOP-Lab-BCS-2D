df6379fc2f51679c5f91251ebe5ac31e824afdee
Amna Ahsan <amnaahsan533@gmail.com>
2025-03-01 16:06:34 +0500

    Update q1.cpp

--- Lab 5/CW/q1.cpp
+++ Lab 5/CW/q1.cpp
@@ -1,1 +1,56 @@
+#include<iostream>
+using namespace std;
 
+class Engine{
+    bool isRunning;
+    public:
+    Engine() : isRunning(false) {
+        cout<<"Engine created"<<endl;
+    }
+    
+    void start(){
+        if(!isRunning){
+            isRunning = true;
+            cout<<"Engine started"<<endl;
+        } else {
+            cout<<"Engine is already runing"<<endl;
+        }      
+    }
+    void stop(){
+        if(isRunning){
+            isRunning = false;
+            cout<<"Engine stopped"<<endl;
+        } else {
+            cout<<"Engine is already stopped"<<endl;
+        }
+         
+    }
+    ~Engine(){
+        cout<<"Engine destroyed"<<endl;
+    }
+};
+
+class Car{
+    Engine engine;
+    public:
+    Car(){
+        cout<<"Car created"<<endl;
+    }
+    void startCar(){
+        engine.start();
+    }
+    void stopCar(){
+        engine.stop();
+    }
+    ~Car(){
+        cout<<"Car destroyed"<<endl;
+    }
+};
+
+
+
+int main(){
+    Car myCar;
+    myCar.startCar();
+    myCar.stopCar();
+}
