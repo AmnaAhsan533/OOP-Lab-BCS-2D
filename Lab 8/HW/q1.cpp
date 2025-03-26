b71173551c91aa3341db42fd124d81db5ef44c9d
Amna Ahsan <amnaahsan533@gmail.com>
2025-03-26 13:55:54 +0500

    Update q1.cpp

--- Lab 8/HW/q1.cpp
+++ Lab 8/HW/q1.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
 
+class Absurd{
+    int x;
+    public:
+   
+    Absurd(int x) : x(x) {}
+    Absurd& operator--(){ //prefix --x
+        x *= 4;
+        return *this;
+    }
+    Absurd operator--(int){ //postfix x--
+        Absurd temp = *this;
+        x /= 4;
+    
+        return temp;
+    }
+    void display(){
+        cout<<"x: "<< x <<endl;
+    }
+};
+
+int main(){
+    Absurd ex(4);
+    cout << "Initial: ";
+    ex.display();
+    --ex;
+    cout << "After --x: ";
+    ex.display();
+    //ex--.display();
+    cout << "After x--: ";
+    ex.display();
+
+    return 0;
+}
