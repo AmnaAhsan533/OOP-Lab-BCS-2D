9fc3463bf2602955f2845e5cf2daf684e6e5003d
Amna Ahsan <amnaahsan533@gmail.com>
2025-04-22 10:03:58 +0500

    Update q1.cpp

--- Lab 10/q1.cpp
+++ Lab 10/q1.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
 
+//ofstream creates a file if not exist and open it in write mode
+
+int main(){
+    string code;
+    cout<<"Enter string: ";
+    getline(cin, code);
+    cout<<"Normal text: "<<code<<endl;
+    ofstream outputFile("File.txt");
+    if(!outputFile){
+        cout<<"Error: File could not be created!"<<endl;
+        return 1;
+    }
+
+    for(int i=0; i<code.size(); i++){
+        code[i] += i+1;
+    }
+
+    outputFile<<code;
+    cout<<"Encrypted text: "<<code<<endl;
+    outputFile.close();
+
+    ifstream inFile("File.txt");
+    if(!inFile.is_open()){
+        cout<<"Unable to open file!"<<endl;
+        return 1;
+    }
+    while(getline(inFile, code)){
+        for(int i=0; i<code.size(); i++){
+            code[i] -= i+1;
+        }
+        cout<< "Decrypted text: "<< code <<endl;
+    }
+    inFile.close();
+
+    return 0;
+}
